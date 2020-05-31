b3f9aef8ad8d6e3d6a2581a22eee2f746dda12ed
mertoz41 <mertozkaynak@windowslive.com>
2020-05-31 15:47:13 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Write your code here!
+let element = document.createElement('div')
+document.body.appendChild(element)
+
+let ul = document.createElement('ul')
+
+for (let i = 0; i < 3; i++) {
+    let li = document.createElement('li')
+    li.innerHTML = (i + 1).toString()
+    ul.appendChild(li)
+}
+
+element.appendChild(ul)
+
+element.style.backgroundColor = '#27647B';
+element.style.textAlign = 'center';
+ul.style.textAlign = 'left';
+let main = document.getElementById('main')
+main.remove()
+
+let newHeader = document.createElement('h1');
+newHeader.setAttribute("id", "victory");
+
+newHeader.innerHTML = "YOUR-NAME is the champion"
+
+
+
+
